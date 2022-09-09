e6a7d239dce7da27ae031a612915881394c21271
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2022-09-09 13:39:03 +0200

    check_cpplint.sh update

--- check_cpplint.sh
+++ check_cpplint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-cpplint --filter=-build/include_subdir ./src/*
-cpplint --filter=-build/include_subdir --recursive ./src/supla/*
-cpplint --filter=-build/include_subdir --recursive ./extras/porting/*
-cpplint --filter=-build/include_subdir,-build/include_order ./extras/examples/linux/*
-cpplint --filter=-build/include_subdir ./extras/examples/esp8266_rtos/main/*
-cpplint --filter=-build/include_subdir ./extras/examples/esp_idf/main/*
-cpplint --filter=-build/include_subdir ./extras/examples/freertos_linux/main.cpp
+EXIT_STATUS=0
+cpplint --filter=-build/include_subdir ./src/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir --recursive ./src/supla/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir --recursive ./extras/porting/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir,-build/include_order ./extras/examples/linux/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir ./extras/examples/esp8266_rtos/main/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir ./extras/examples/esp_idf/main/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir ./extras/examples/freertos_linux/main.cpp || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]
+then
+  echo
+  echo "======================="
+  echo "| ERRORS: please fix! |"
+  echo "======================="
+fi
+
+exit $EXIT_STATUS

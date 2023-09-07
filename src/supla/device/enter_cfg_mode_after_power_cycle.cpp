4e09a134be79883d517e0cf39a18d20fac0d1c7a
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2023-09-07 13:29:10 +0200

    EnterCfgModeAfterPowerCycle: add class to trigger cfg mode after few quick power cycles

---
+++ src/supla/device/enter_cfg_mode_after_power_cycle.cpp
@@ -0,0 +1,84 @@
+/*
+   Copyright (C) AC SOFTWARE SP. Z O.O
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+   */
+
+#include "enter_cfg_mode_after_power_cycle.h"
+#include <SuplaDevice.h>
+#include <supla/storage/config.h>
+#include <supla/log_wrapper.h>
+#include <supla/time.h>
+#include <supla/network/html/disable_user_interface_parameter.h>
+
+using Supla::Device::EnterCfgModeAfterPowerCycle;
+
+namespace Supla::Device {
+const char PowerCycleKey[] = "power_cycle";
+}  // namespace Supla::Device
+
+EnterCfgModeAfterPowerCycle::EnterCfgModeAfterPowerCycle(uint32_t timeoutMs,
+                                                         int powerCycles)
+    : timeoutMs(timeoutMs), maxPowerCycles(powerCycles) {
+}
+
+void EnterCfgModeAfterPowerCycle::onLoadConfig(SuplaDeviceClass *sdc) {
+  this->sdc = sdc;
+  timestampMs = millis();
+  if (timestampMs == 0) {
+    timestampMs = 1;
+  }
+  auto cfg = Supla::Storage::ConfigInstance();
+  if (cfg) {
+    uint8_t disableUI = 0;
+    cfg->getUInt8(Supla::Html::DisableUserInterfaceCfgTag, &disableUI);
+    enabled = (disableUI == 1);
+
+    cfg->getUInt32(Supla::Device::PowerCycleKey, &currentPowerCycle);
+    SUPLA_LOG_DEBUG("PowerCycle: %d", currentPowerCycle);
+  }
+}
+
+void EnterCfgModeAfterPowerCycle::iterateAlways() {
+  if (!enabled || timestampMs == 0) {
+    return;
+  }
+
+  if (!incremented) {
+    currentPowerCycle++;
+
+    if (currentPowerCycle > maxPowerCycles) {
+      sdc->enterConfigMode();
+    } else {
+      auto cfg = Supla::Storage::ConfigInstance();
+      if (cfg) {
+        cfg->setUInt32(Supla::Device::PowerCycleKey, currentPowerCycle);
+        cfg->commit();
+      }
+    }
+    incremented = true;
+  }
+
+  if (millis() - timestampMs > timeoutMs) {
+    currentPowerCycle = 0;
+    timestampMs = 0;
+    SUPLA_LOG_DEBUG("PowerCycle: timeout, counter reset to 0");
+    auto cfg = Supla::Storage::ConfigInstance();
+    if (cfg) {
+      cfg->setUInt32(Supla::Device::PowerCycleKey, currentPowerCycle);
+      cfg->saveWithDelay(1000);
+    }
+  }
+}
---
+++ src/supla/device/enter_cfg_mode_after_power_cycle.h
@@ -0,0 +1,47 @@
+/*
+   Copyright (C) AC SOFTWARE SP. Z O.O
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+   */
+
+#ifndef SRC_SUPLA_DEVICE_ENTER_CFG_MODE_AFTER_POWER_CYCLE_H_
+#define SRC_SUPLA_DEVICE_ENTER_CFG_MODE_AFTER_POWER_CYCLE_H_
+
+#include <supla/element.h>
+
+class SuplaDeviceClass;
+
+namespace Supla::Device {
+class EnterCfgModeAfterPowerCycle : public Supla::Element {
+ public:
+  explicit EnterCfgModeAfterPowerCycle(uint32_t timeoutMs = 5000,
+                                       int powerCycles = 3);
+
+  void onLoadConfig(SuplaDeviceClass *sdc) override;
+  void iterateAlways() override;
+
+ private:
+  SuplaDeviceClass *sdc = nullptr;
+  uint32_t timestampMs = 0;
+  uint32_t timeoutMs = 5000;
+  int maxPowerCycles = 3;
+  uint32_t currentPowerCycle = 0;
+  bool incremented = false;
+  bool enabled = false;
+};
+
+}  // namespace Supla::Device
+
+#endif  // SRC_SUPLA_DEVICE_ENTER_CFG_MODE_AFTER_POWER_CYCLE_H_

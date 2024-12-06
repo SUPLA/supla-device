20a72c9e385a3ce94437af3c869705de162effd5
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2024-12-06 11:45:56 +0100

    Another fix for not sending channel state info autonomously when only battery level is configured

--- src/supla/element.cpp
+++ src/supla/element.cpp
@@ -229,13 +229,13 @@ void Element::handleGetChannelState(TDSC_ChannelState *channelState) {
       if (channel->isBatteryPoweredFieldEnabled()) {
         channelState->Fields |= SUPLA_CHANNELSTATE_FIELD_BATTERYPOWERED;
         channelState->BatteryPowered = channel->isBatteryPowered() ? 1 : 0;
+      }
 
-        channelState->BatteryLevel = channel->getBatteryLevel();
-        if (channelState->BatteryLevel <= 100) {
-          channelState->Fields |= SUPLA_CHANNELSTATE_FIELD_BATTERYLEVEL;
-        } else {
-          channelState->BatteryLevel = 0;
-        }
+      channelState->BatteryLevel = channel->getBatteryLevel();
+      if (channelState->BatteryLevel <= 100) {
+        channelState->Fields |= SUPLA_CHANNELSTATE_FIELD_BATTERYLEVEL;
+      } else {
+        channelState->BatteryLevel = 0;
       }
       if (channel->isBridgeSignalStrengthAvailable()) {
         channelState->Fields |=

cf489003de642886412bf3f60d19f9483ef7f45b
klew <klew@users.noreply.github.com>
2025-02-12 07:55:44 +0000

    deploy: e4ea8a3d4a9145558a698b37195a9b66044cbfcd

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['clearinvalidptr_0',['ClearInvalidPtr',['../classSupla_1_1Element.html#a59b914551b75cc301fb47dfc2c9f4115',1,'Supla::Element']]],
-  ['closevalve_1',['closeValve',['../classSupla_1_1Control_1_1ValveBase.html#a016316e194423fd6566466e99ce2c5a6',1,'Supla::Control::ValveBase::closeValve()'],['../classValveBase.html#a016316e194423fd6566466e99ce2c5a6',1,'ValveBase::closeValve()']]]
+  ['clearcurrentphasesequenceflag_0',['clearCurrentPhaseSequenceFlag',['../classSupla_1_1Sensor_1_1ElectricityMeter.html#a41e1ea07a89e920dab8d1b5969758674',1,'Supla::Sensor::ElectricityMeter::clearCurrentPhaseSequenceFlag()'],['../classElectricityMeter.html#a41e1ea07a89e920dab8d1b5969758674',1,'ElectricityMeter::clearCurrentPhaseSequenceFlag()']]],
+  ['clearinvalidptr_1',['ClearInvalidPtr',['../classSupla_1_1Element.html#a59b914551b75cc301fb47dfc2c9f4115',1,'Supla::Element']]],
+  ['clearvoltagephasesequenceflag_2',['clearVoltagePhaseSequenceFlag',['../classSupla_1_1Sensor_1_1ElectricityMeter.html#ac0b73024e76a885d2869ed1001d29159',1,'Supla::Sensor::ElectricityMeter::clearVoltagePhaseSequenceFlag()'],['../classElectricityMeter.html#ac0b73024e76a885d2869ed1001d29159',1,'ElectricityMeter::clearVoltagePhaseSequenceFlag()']]],
+  ['closevalve_3',['closeValve',['../classSupla_1_1Control_1_1ValveBase.html#a016316e194423fd6566466e99ce2c5a6',1,'Supla::Control::ValveBase::closeValve()'],['../classValveBase.html#a016316e194423fd6566466e99ce2c5a6',1,'ValveBase::closeValve()']]]
 ];

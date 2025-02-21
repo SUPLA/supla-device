59bcf4799bb7006c8a76eb9c900bab85cad36f00
petione <33701246+petione@users.noreply.github.com>
2025-02-21 22:29:07 +0100

    Button update (#87)
    Html: add ButtonUpdate class which adds button on local WebInterface for OTA software updates with direct file upload.

---
+++ src/supla/network/html/button_update.cpp
@@ -0,0 +1,47 @@
+/*
+ Copyright (C) Petione for AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+#ifndef ARDUINO_ARCH_AVR  // Wykluczenie dla platformy AVR (Arduino Mega)
+#include "button_update.h"
+#include <supla/network/web_sender.h>
+// Include the full header for EspWebServer
+#include <supla/network/esp_web_server.h>
+
+using Supla::Html::ButtonUpdate;
+
+ButtonUpdate::ButtonUpdate(Supla::EspWebServer* server)
+    : HtmlElement(HTML_SECTION_BUTTON_AFTER), server(server) {
+// Dynamically create the httpUpdater object
+#ifdef ARDUINO_ARCH_ESP32
+  httpUpdater = new HTTPUpdateServer();
+#else
+  httpUpdater = new ESP8266HTTPUpdateServer();
+#endif
+  // Automatic OTA update setup
+  httpUpdater->setup(server->getServerPtr(), "/update");
+}
+
+ButtonUpdate::~ButtonUpdate() {
+  // Delete the object to free memory
+  delete httpUpdater;
+}
+
+void ButtonUpdate::send(Supla::WebSender* sender) {
+  sender->send(
+      ("<button type=\"button\" onclick=\"window.location.href='/update';\">"
+       "UPDATE"
+       "</button>"));
+}
+#endif
---
+++ src/supla/network/html/button_update.h
@@ -0,0 +1,54 @@
+/*
+ Copyright (C) Petione for AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef SRC_SUPLA_NETWORK_HTML_BUTTON_UPDATE_H_
+#define SRC_SUPLA_NETWORK_HTML_BUTTON_UPDATE_H_
+#include <supla/network/html_element.h>
+
+// Forward declaration of EspWebServer class
+namespace Supla {
+class EspWebServer;
+}  // namespace Supla
+
+#ifdef ARDUINO_ARCH_ESP32
+#include <HTTPUpdateServer.h>
+#else
+#include <ESP8266HTTPUpdateServer.h>
+#endif
+
+namespace Supla {
+namespace Html {
+
+class ButtonUpdate : public HtmlElement {
+ private:
+  Supla::EspWebServer* server = nullptr;  // Pointer to the server, initialized to nullptr
+
+#ifdef ARDUINO_ARCH_ESP32
+  HTTPUpdateServer* httpUpdater = nullptr;
+#else
+  ESP8266HTTPUpdateServer* httpUpdater = nullptr;
+#endif
+
+ public:
+  explicit ButtonUpdate(Supla::EspWebServer* server);  // Marked as explicit
+  ~ButtonUpdate();  // Destructor to free memory
+  void send(Supla::WebSender* sender) override;
+};
+
+};  // namespace Html
+};  // namespace Supla
+
+#endif  // SRC_SUPLA_NETWORK_HTML_BUTTON_UPDATE_H_

3f3dcdc994b0653f40c1ea211aa7fac197280d8a
malarz <malarz@batory.edu.pl>
2025-02-27 20:13:41 +0100

    Update pm1006k.cpp

--- src/supla/sensor/pm1006k.cpp
+++ src/supla/sensor/pm1006k.cpp
@@ -16,7 +16,7 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
    */
 
-#include "supla_sensor_pm1006k.h"
+#include "pm1006k.h"
 
 #include <supla/log_wrapper.h>
 

af71fd5d34efa3bfc33f1e0687c25dde6206763e
klew <klew@users.noreply.github.com>
2025-02-28 09:47:24 +0000

    deploy: be8b6a8e46cd9be86487d7005af8d68b31323374

--- search/classes_9.js
+++ search/classes_9.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['ieeefloat_0',['IEEEfloat',['../structIEEEfloat.html',1,'']]],
-  ['impulsecounter_1',['ImpulseCounter',['../classImpulseCounter.html',1,'ImpulseCounter'],['../classSupla_1_1Sensor_1_1ImpulseCounter.html',1,'Supla::Sensor::ImpulseCounter']]],
-  ['internalpinoutput_2',['InternalPinOutput',['../classSupla_1_1Control_1_1InternalPinOutput.html',1,'Supla::Control']]],
-  ['io_3',['Io',['../classSupla_1_1Io.html',1,'Supla']]],
-  ['ipaddress_4',['IPAddress',['../classIPAddress.html',1,'']]]
+  ['i2cscanner_0',['I2Cscanner',['../classSupla_1_1Html_1_1I2Cscanner.html',1,'Supla::Html']]],
+  ['ieeefloat_1',['IEEEfloat',['../structIEEEfloat.html',1,'']]],
+  ['impulsecounter_2',['ImpulseCounter',['../classImpulseCounter.html',1,'ImpulseCounter'],['../classSupla_1_1Sensor_1_1ImpulseCounter.html',1,'Supla::Sensor::ImpulseCounter']]],
+  ['internalpinoutput_3',['InternalPinOutput',['../classSupla_1_1Control_1_1InternalPinOutput.html',1,'Supla::Control']]],
+  ['io_4',['Io',['../classSupla_1_1Io.html',1,'Supla']]],
+  ['ipaddress_5',['IPAddress',['../classIPAddress.html',1,'']]]
 ];

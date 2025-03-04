85bad36e84728a5dc70b3d207b8aa338200609d3
malarz <malarz@batory.edu.pl>
2025-03-04 17:20:37 +0100

    Update pm1006k.h

--- src/supla/sensor/pm1006k.h
+++ src/supla/sensor/pm1006k.h
@@ -32,11 +32,81 @@ class pm1006k : public GeneralPurposeMeasurement {
  public:
   // rx_pin, tx_pin: pins to which the sensor is connected
   // refresh: time between readings (in minutes: 1-1440)
-  explicit pm1006k(int rx_pin, int tx_pin, int fan_pin = 0, int refresh = 10);
+  explicit pm1006k(int rx_pin, int tx_pin, int fan_pin = 0, int refresh = 10)
+      : GeneralPurposeMeasurement(nullptr, false) {
+    if (refresh < 1) {
+      refresh = 10;
+    } else if (refresh > 1440) {
+      refresh = 10;
+    }
 
-  void onInit() override;
-  void iterateAlways() override;
-  double getValue() override;
+    // FAN setup
+    fanPin = fan_pin;
+    if (fanPin) {
+      pinMode(fanPin, OUTPUT);
+      fanOff = false;
+      digitalWrite(fanPin, HIGH);
+      SUPLA_LOG_DEBUG("PM1006K FAN: started & on");
+    }
+
+    // Setup and create instance of the PM1006K driver
+    // The baud rate for the serial connection must be PM1006K::BAUD_RATE.
+    Serial1.begin(PM1006K::BAUD_RATE, SERIAL_8N1, rx_pin, tx_pin);
+    sensor = new PM1006K(&Serial1);
+
+    refreshIntervalMs = refresh * 60 * 1000;
+    setDefaultUnitAfterValue("μg/m³");
+    setInitialCaption("PM 2.5");
+    getChannel()->setDefaultIcon(8);
+  }
+
+  void onInit() override {
+    iterateAlways();
+  }
+
+  void iterateAlways() override {
+    // 15 sec befor reading sensor
+    if (millis() - lastReadTime > refreshIntervalMs-15000) {
+      if (fanPin && fanOff) {
+        fanOff = false;
+        digitalWrite(fanPin, HIGH);
+        SUPLA_LOG_DEBUG("PM1006K FAN: on");
+      }
+    }
+
+    if (millis() - lastReadTime > refreshIntervalMs) {
+      int32_t value = NAN;
+      if (!sensor->takeMeasurement()) {
+        SUPLA_LOG_DEBUG("PM1006K: failed to take measurement");
+      } else {
+        value = sensor->getPM2_5();
+        SUPLA_LOG_DEBUG("PM1006K: read: %d", value);
+      }
+
+      if (fanPin) {
+        fanOff = true;
+        digitalWrite(fanPin, LOW);
+        SUPLA_LOG_DEBUG("PM1006K FAN: off");
+      }
+
+      if (isnan(value) || value <= 0) {
+        if (invalidCounter < 3) {
+          invalidCounter++;
+        } else {
+          lastValue = NAN;
+        }
+      } else {
+        invalidCounter = 0;
+        lastValue = value;
+      }
+      channel.setNewValue(getValue());
+      lastReadTime = millis();
+    }
+  }
+
+  double getValue() override {
+    return lastValue;
+  }
 
  protected:
   ::PM1006K* sensor;
@@ -44,7 +114,7 @@ class pm1006k : public GeneralPurposeMeasurement {
   uint32_t lastReadTime = 0;
   uint32_t lastValue = NAN;
   int fanPin = 0;
-  bool fanOff = false;
+  bool fanOff = true;
   int invalidCounter = 0;
 };
 

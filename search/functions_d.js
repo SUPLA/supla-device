4834e67b541ddc7f4ff67d27c6e47e94f001b548
klew <klew@users.noreply.github.com>
2025-04-01 07:33:35 +0000

    deploy: a47fa4f3066f64af284e53f8fc96412c86481430

--- search/functions_d.js
+++ search/functions_d.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['removesensor_0',['removeSensor',['../classSupla_1_1Control_1_1ValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'Supla::Control::ValveBase::removeSensor()'],['../classValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'ValveBase::removeSensor()']]]
+  ['removesensor_0',['removeSensor',['../classSupla_1_1Control_1_1ValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'Supla::Control::ValveBase::removeSensor()'],['../classValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'ValveBase::removeSensor()']]],
+  ['removesensordata_1',['removeSensorData',['../classSupla_1_1Sensor_1_1Container.html#ac97e08bfea047cd89d7af8ea2fe274fc',1,'Supla::Sensor::Container::removeSensorData()'],['../classContainer.html#ac97e08bfea047cd89d7af8ea2fe274fc',1,'Container::removeSensorData()']]]
 ];

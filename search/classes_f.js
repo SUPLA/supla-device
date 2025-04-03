f00ba4baeb011c0e2c6944a5f1572443998acb0f
klew <klew@users.noreply.github.com>
2025-04-03 09:16:01 +0000

    deploy: 289001e2f1ac29487b388e92141190cb388a22f7

--- search/classes_f.js
+++ search/classes_f.js
@@ -1,14 +1,16 @@
 var searchData=
 [
-  ['pinstatusled_0',['PinStatusLed',['../classSupla_1_1Control_1_1PinStatusLed.html',1,'Supla::Control']]],
-  ['poweractivewgetter_1',['PowerActiveWGetter',['../classSupla_1_1PowerActiveWGetter.html',1,'Supla']]],
-  ['powerapparentvagetter_2',['PowerApparentVAGetter',['../classSupla_1_1PowerApparentVAGetter.html',1,'Supla']]],
-  ['powerreactivevargetter_3',['PowerReactiveVarGetter',['../classSupla_1_1PowerReactiveVarGetter.html',1,'Supla']]],
-  ['powerstatusledparameters_4',['PowerStatusLedParameters',['../classPowerStatusLedParameters.html',1,'PowerStatusLedParameters'],['../classSupla_1_1Html_1_1PowerStatusLedParameters.html',1,'Supla::Html::PowerStatusLedParameters']]],
-  ['preamble_5',['Preamble',['../structSupla_1_1Preamble.html',1,'Supla']]],
-  ['pressure_6',['Pressure',['../classSupla_1_1Sensor_1_1Pressure.html',1,'Supla::Sensor']]],
-  ['protocollayer_7',['ProtocolLayer',['../classSupla_1_1Protocol_1_1ProtocolLayer.html',1,'Supla::Protocol']]],
-  ['protocolparameters_8',['ProtocolParameters',['../classSupla_1_1Html_1_1ProtocolParameters.html',1,'Supla::Html']]],
-  ['pzemv2_9',['PZEMv2',['../classSupla_1_1Sensor_1_1PZEMv2.html',1,'Supla::Sensor']]],
-  ['pzemv3_10',['PZEMv3',['../classSupla_1_1Sensor_1_1PZEMv3.html',1,'Supla::Sensor']]]
+  ['particlemeter_0',['ParticleMeter',['../classSupla_1_1Sensor_1_1ParticleMeter.html',1,'Supla::Sensor']]],
+  ['particlemeterpm1006k_1',['ParticleMeterPM1006K',['../classSupla_1_1Sensor_1_1ParticleMeterPM1006K.html',1,'Supla::Sensor']]],
+  ['pinstatusled_2',['PinStatusLed',['../classSupla_1_1Control_1_1PinStatusLed.html',1,'Supla::Control']]],
+  ['poweractivewgetter_3',['PowerActiveWGetter',['../classSupla_1_1PowerActiveWGetter.html',1,'Supla']]],
+  ['powerapparentvagetter_4',['PowerApparentVAGetter',['../classSupla_1_1PowerApparentVAGetter.html',1,'Supla']]],
+  ['powerreactivevargetter_5',['PowerReactiveVarGetter',['../classSupla_1_1PowerReactiveVarGetter.html',1,'Supla']]],
+  ['powerstatusledparameters_6',['PowerStatusLedParameters',['../classPowerStatusLedParameters.html',1,'PowerStatusLedParameters'],['../classSupla_1_1Html_1_1PowerStatusLedParameters.html',1,'Supla::Html::PowerStatusLedParameters']]],
+  ['preamble_7',['Preamble',['../structSupla_1_1Preamble.html',1,'Supla']]],
+  ['pressure_8',['Pressure',['../classSupla_1_1Sensor_1_1Pressure.html',1,'Supla::Sensor']]],
+  ['protocollayer_9',['ProtocolLayer',['../classSupla_1_1Protocol_1_1ProtocolLayer.html',1,'Supla::Protocol']]],
+  ['protocolparameters_10',['ProtocolParameters',['../classSupla_1_1Html_1_1ProtocolParameters.html',1,'Supla::Html']]],
+  ['pzemv2_11',['PZEMv2',['../classSupla_1_1Sensor_1_1PZEMv2.html',1,'Supla::Sensor']]],
+  ['pzemv3_12',['PZEMv3',['../classSupla_1_1Sensor_1_1PZEMv3.html',1,'Supla::Sensor']]]
 ];

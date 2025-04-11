80df3e059055a1c6336acf77c42031b0eb644d69
klew <klew@users.noreply.github.com>
2025-04-11 07:52:08 +0000

    deploy: 7f4535005c79cf3ca98c84d8d930f9c435252d3d

--- search/functions_d.js
+++ search/functions_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['removesensor_0',['removeSensor',['../classSupla_1_1Control_1_1ValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'Supla::Control::ValveBase::removeSensor()'],['../classValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'ValveBase::removeSensor()']]],
-  ['removesensordata_1',['removeSensorData',['../classSupla_1_1Sensor_1_1Container.html#ac97e08bfea047cd89d7af8ea2fe274fc',1,'Supla::Sensor::Container::removeSensorData()'],['../classContainer.html#ac97e08bfea047cd89d7af8ea2fe274fc',1,'Container::removeSensorData()']]]
+  ['remoteoutputinterface_0',['RemoteOutputInterface',['../classSupla_1_1Control_1_1RemoteOutputInterface.html#ae6dbfc3de7c7cf8a1d55e3ab78952155',1,'Supla::Control::RemoteOutputInterface::RemoteOutputInterface()'],['../classRemoteOutputInterface.html#ae6dbfc3de7c7cf8a1d55e3ab78952155',1,'RemoteOutputInterface::RemoteOutputInterface()']]],
+  ['removesensor_1',['removeSensor',['../classSupla_1_1Control_1_1ValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'Supla::Control::ValveBase::removeSensor()'],['../classValveBase.html#a1219856ed53ce43581a10a5853110fab',1,'ValveBase::removeSensor()']]],
+  ['removesensordata_2',['removeSensorData',['../classSupla_1_1Sensor_1_1Container.html#ac97e08bfea047cd89d7af8ea2fe274fc',1,'Supla::Sensor::Container::removeSensorData()'],['../classContainer.html#ac97e08bfea047cd89d7af8ea2fe274fc',1,'Container::removeSensorData()']]]
 ];

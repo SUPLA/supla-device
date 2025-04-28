c0f6aee4005bed1b107a6975c82e9422bf236670
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2025-04-28 20:01:39 +0200

    Arduino IDE: add VirtualBinary example

---
+++ examples/VirtualRelay/VirtualRelay.ino
@@ -0,0 +1,67 @@
+/*
+  Copyright (C) AC SOFTWARE SP. Z O.O.
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#define STATUS_LED_GPIO 2
+
+#include <SuplaDevice.h>
+#include <supla/network/esp_wifi.h>
+#include <supla/device/status_led.h>
+#include <supla/storage/littlefs_config.h>
+#include <supla/network/esp_web_server.h>
+#include <supla/network/html/device_info.h>
+#include <supla/network/html/protocol_parameters.h>
+#include <supla/network/html/status_led_parameters.h>
+#include <supla/network/html/wifi_parameters.h>
+#include <supla/device/supla_ca_cert.h>
+#include <supla/control/virtual_relay.h>
+#include <supla/storage/eeprom.h>
+
+Supla::Eeprom eeprom;
+
+Supla::ESPWifi wifi;
+Supla::LittleFsConfig configSupla;
+
+Supla::Device::StatusLed statusLed(STATUS_LED_GPIO, true); // inverted state
+Supla::EspWebServer suplaServer;
+
+void setup() {
+  Serial.begin(115200);
+
+  // HTML www component
+  new Supla::Html::DeviceInfo(&SuplaDevice);
+  new Supla::Html::WifiParameters;
+  new Supla::Html::ProtocolParameters;
+  new Supla::Html::StatusLedParameters;
+
+  // Channel #0 Virtual Relay
+  auto vr = new Supla::Control::VirtualRelay();
+  vr->setDefaultFunction(SUPLA_CHANNELFNC_POWERSWITCH);
+  // Enable state restore after device restart:
+  vr->setDefaultStateRestore();
+
+  // configure defualt Supla CA certificate
+  SuplaDevice.setSuplaCACert(suplaCACert);
+  SuplaDevice.setSupla3rdPartyCACert(supla3rdCACert);
+
+  SuplaDevice.begin();
+}
+
+void loop() {
+  SuplaDevice.iterate();
+}
+

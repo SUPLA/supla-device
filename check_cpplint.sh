36dc1616220df0b939ddc498a52042eed3442047
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2025-07-14 12:36:41 +0200

    Tools: cpp lint check script update --quiet

--- check_cpplint.sh
+++ check_cpplint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 EXIT_STATUS=0
-cpplint --filter=-build/include_subdir ./src/* || EXIT_STATUS=$?
-cpplint --filter=-build/include_subdir --recursive ./src/supla/* || EXIT_STATUS=$?
-cpplint --filter=-build/include_subdir --recursive ./extras/porting/* || EXIT_STATUS=$?
-cpplint --filter=-build/include_subdir,-build/include_order ./extras/examples/linux/* || EXIT_STATUS=$?
-cpplint --filter=-build/include_subdir ./extras/examples/esp8266_rtos/main/* || EXIT_STATUS=$?
-cpplint --filter=-build/include_subdir ./extras/examples/esp_idf/main/* || EXIT_STATUS=$?
-cpplint --filter=-build/include_subdir ./extras/examples/freertos_linux/main.cpp || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir --quiet ./src/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir --quiet --recursive ./src/supla/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir --quiet --recursive ./extras/porting/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir,-build/include_order --quiet ./extras/examples/linux/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir --quiet ./extras/examples/esp8266_rtos/main/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir --quiet ./extras/examples/esp_idf/main/* || EXIT_STATUS=$?
+cpplint --filter=-build/include_subdir --quiet ./extras/examples/freertos_linux/main.cpp || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]
 then

0a42ecf04b529454adbf6b0d1e43dd513b5efa07
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2025-08-21 11:54:35 +0200

    Version increment 25.08.01

--- src/supla/version.h
+++ src/supla/version.h
@@ -5,10 +5,12 @@
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
  of the License, or (at your option) any later version.
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
+
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -17,7 +19,7 @@
 #ifndef SRC_SUPLA_VERSION_H_
 #define SRC_SUPLA_VERSION_H_
 
-#define SUPLA_SHORT_VERSION "25.08"
+#define SUPLA_SHORT_VERSION "25.08.01"
 const char suplaDeviceVersion[] = "SDK " SUPLA_SHORT_VERSION "-dev";
 
 #endif  // SRC_SUPLA_VERSION_H_

8173c92fe2304f3f47e969cbe789250da22319db
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2025-09-19 15:59:09 +0200

    Arduino mega compilation fix - skip web sender compilation

--- src/supla/network/web_sender.cpp
+++ src/supla/network/web_sender.cpp
@@ -16,6 +16,8 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#ifndef ARDUINO_ARCH_AVR
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
@@ -154,3 +156,5 @@ void WebSender::sendTimestamp(uint32_t timestamp) {
 }
 
 };  // namespace Supla
+
+#endif

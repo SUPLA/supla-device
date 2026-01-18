45dc45ae654ce00de1387904b93bd77105205408
malarz <malarz@batory.edu.pl>
2026-01-18 20:44:17 +0100

    intends

--- src/supla/control/AddressableLEDs.h
+++ src/supla/control/AddressableLEDs.h
@@ -29,7 +29,7 @@
 
 namespace Supla {
 namespace Control {
-  
+
 enum AddressableLEDsEffect : uint8_t {
   VEEROOS,
   SWAP,
@@ -40,243 +40,244 @@ enum AddressableLEDsEffect : uint8_t {
 };
 
 class AddressableLEDs : public Supla::Element {
-  public:
-    AddressableLEDs(uint16_t number, int16_t pin, neoPixelType type = NEO_GRB + NEO_KHZ800) {
-      numberOfLeds = number;
-      pixels = new Adafruit_NeoPixel(numberOfLeds, pin, type);
+ public:
+  AddressableLEDs(uint16_t number, int16_t pin, 
+         neoPixelType type = NEO_GRB + NEO_KHZ800) {
+    numberOfLeds = number;
+    pixels = new Adafruit_NeoPixel(numberOfLeds, pin, type);
 
-      pixels->begin();
+    pixels->begin();
+  }
+
+  void setEffect(AddressableLEDsEffect _effect, int time) {
+    if (effect != _effect) {
+      effect = _effect;
+      counter = 0;
+
+      // switching OFF all LEDs
+      for (int i=0; i<numberOfLeds; i++) {
+        pixels->setPixelColor(i, 0);
+      }
+      pixels->show();
+      lightedLeds = 0;
     }
+    if (OneLedTime != time) { 
+       OneLedTime = time;
+    }
+  }
 
-    void setEffect(AddressableLEDsEffect _effect, int time) {
-      if (effect != _effect) {
-        effect = _effect;
-        counter = 0;
+  AddressableLEDsEffect getEffect() {
+    return effect;
+  }
 
-        // switching OFF all LEDs
-        for (int i=0; i<numberOfLeds; i++) {
+  bool isOn() {
+    return on;
+  }
+  void turnOn() {
+    on = true;
+  }
+  void turnOff() {
+    on = false;
+  }
+
+  void setColor(uint32_t color) {
+    RGBcolor = color;
+  }
+  void setColor(uint8_t red, uint8_t green, uint8_t blue) {
+    RGBcolor = pixels->Color(red, green, blue);
+  }
+  void setBrightness(uint8_t brightness) {
+    pixels->setBrightness(brightness);
+  }
+
+  void iterateAlways() {
+    switch(effect) {
+      case VEEROOS:
+        iterateAlways_Veeroos();
+        break;
+      case SWAP:
+        iterateAlways_Swap();
+        break;
+      case FLOW:
+        iterateAlways_Flow();
+        break;
+      case RAINBOWWHEEL:
+        iterateAlways_RainbowWheel();
+        break;
+      case RAINBOW:
+        lightedLeds = numberOfLeds;
+        iterateAlways_Rainbow();
+        break;
+      case RAINBOWVEEROOS:
+        iterateAlways_RainbowVeeroos();
+        break;
+    }
+  }
+
+ protected:
+  Adafruit_NeoPixel *pixels;
+  bool on = false;
+  uint16_t numberOfLeds;
+  uint16_t lightedLeds = 0;
+  uint32_t RGBcolor = 0x004400; // color of SUPLA :-)
+  uint32_t LastColor = 0;
+
+  int OneLedTime = 200;
+  uint32_t lastTime = 0;
+
+  AddressableLEDsEffect effect = VEEROOS;
+  long counter = 0;
+
+  void iterateAlways_Veeroos() {
+    // LEDs switching ON
+    if (isOn() && lightedLeds<numberOfLeds && (millis()-lastTime >= OneLedTime) || (millis() < lastTime)) {
+      lastTime = millis();
+      SUPLA_LOG_DEBUG("RGB strip: switching on LED %d with color 0x%06x", lightedLeds, RGBcolor);
+      pixels->setPixelColor(lightedLeds, RGBcolor);
+      pixels->show();
+      lightedLeds++;
+    }
+    // LEDs changing colour
+    if (LastColor != RGBcolor) {
+      LastColor = RGBcolor;
+      for (int i=0; i<lightedLeds; i++) {
+        pixels->setPixelColor(i, RGBcolor);
+      }
+      pixels->show();
+    }
+    // LEDs switching OFF
+    if (!isOn() && lightedLeds>0 && (millis()-lastTime >= OneLedTime) || (millis() < lastTime)) {
+      lastTime = millis();
+      lightedLeds--;
+      SUPLA_LOG_DEBUG("RGB strip: switching off LED %d with color 0x%06x", lightedLeds, RGBcolor);
+      pixels->setPixelColor(lightedLeds, 0);
+      pixels->show();
+    }
+  }
+
+  void iterateAlways_Swap() {
+    if (isOn() && ((millis()-lastTime >= OneLedTime) || (millis() < lastTime))) {
+      lastTime = millis();
+      counter++;
+      for (int i=0; i<numberOfLeds; i++) {
+        if ((i+counter)%2) {
+          pixels->setPixelColor(i, RGBcolor);
+        } else {
           pixels->setPixelColor(i, 0);
         }
-        pixels->show();
-        lightedLeds = 0;
       }
-      if (OneLedTime != time) { 
-        OneLedTime = time;
+      pixels->show();
+    }
+    if (!isOn()) {
+      for (int i=0; i<numberOfLeds; i++) {
+        pixels->setPixelColor(i, 0);
       }
+      pixels->show();
     }
+  } 
 
-    AddressableLEDsEffect getEffect() {
-      return effect;
-    }
-
-    bool isOn() {
-      return on;
-    }
-    void turnOn() {
-      on = true;
-    }
-    void turnOff() {
-      on = false;
-    }
-
-    void setColor(uint32_t color) {
-      RGBcolor = color;
-    }
-    void setColor(uint8_t red, uint8_t green, uint8_t blue) {
-      RGBcolor = pixels->Color(red, green, blue);
-    }
-    void setBrightness(uint8_t brightness) {
-      pixels->setBrightness(brightness);
-    }
-
-    void iterateAlways() {
-      switch(effect) {
-        case VEEROOS:
-          iterateAlways_Veeroos();
-          break;
-        case SWAP:
-          iterateAlways_Swap();
-          break;
-        case FLOW:
-          iterateAlways_Flow();
-          break;
-        case RAINBOWWHEEL:
-          iterateAlways_RainbowWheel();
-          break;
-        case RAINBOW:
-          lightedLeds = numberOfLeds;
-          iterateAlways_Rainbow();
-          break;
-        case RAINBOWVEEROOS:
-          iterateAlways_RainbowVeeroos();
-          break;
+  void iterateAlways_Flow() {
+    if (isOn() && ((millis()-lastTime >= OneLedTime) || (millis() < lastTime))) {
+      lastTime = millis();
+      counter++;
+      for (int i=0; i<numberOfLeds; i++) {
+        if ((i+counter)%4) {
+          pixels->setPixelColor(i, RGBcolor);
+        } else {
+          pixels->setPixelColor(i, 0);
+        }
       }
+      pixels->show();
     }
+    if (!isOn()) {
+      for (int i=0; i<numberOfLeds; i++) {
+        pixels->setPixelColor(i, 0);
+      }
+      pixels->show();
+    }
+  } 
 
-  protected:
-    Adafruit_NeoPixel *pixels;
-    bool on = false;
-    uint16_t numberOfLeds;
-    uint16_t lightedLeds = 0;
-    uint32_t RGBcolor = 0x004400; // color of SUPLA :-)
-    uint32_t LastColor = 0;
+  void iterateAlways_RainbowWheel() {
+    if (isOn() && ((millis()-lastTime >= OneLedTime) || (millis() < lastTime))) {
+      lastTime = millis();
+      counter++;
+      for (int i=0; i<numberOfLeds; i++) {
+        if (counter>255) {
+          counter = 0;
+        }
+        pixels->setPixelColor(i, RainbowWheel((i*1+counter) & 255));
+      }
+      pixels->show();
+    }
+    if (!isOn()) {
+      for (int i=0; i<numberOfLeds; i++) {
+        pixels->setPixelColor(i, 0);
+      }
+      pixels->show();
+    }
+  } 
 
-    int OneLedTime = 200;
-    uint32_t lastTime = 0;
+  void iterateAlways_Rainbow() {
+    if (isOn() && ((millis()-lastTime >= OneLedTime) || (millis() < lastTime))) {
+      lastTime = millis();
+      counter++;
+      for (int i=0; i<lightedLeds; i++) {
+        if (counter*256 > 5*65536) {
+          counter = 0;
+        }
+        int pixelHue = 256*counter + (i * 65536L / numberOfLeds);
+        pixels->setPixelColor(i, pixels->gamma32(pixels->ColorHSV(pixelHue)));
+      }
+      pixels->show();
+    }
+    if (!isOn()) {
+      for (int i=0; i<numberOfLeds; i++) {
+        pixels->setPixelColor(i, 0);
+      }
+      pixels->show();
+    }
+  } 
 
-    AddressableLEDsEffect effect = VEEROOS;
-    long counter = 0;
+  void iterateAlways_RainbowVeeroos() {
+    if ((millis()-lastTime >= OneLedTime) || (millis() < lastTime)) {
+      lastTime = millis();
+      counter++;
 
-    void iterateAlways_Veeroos() {
       // LEDs switching ON
-      if (isOn() && lightedLeds<numberOfLeds && (millis()-lastTime >= OneLedTime) || (millis() < lastTime)) {
-        lastTime = millis();
-        SUPLA_LOG_DEBUG("RGB strip: switching on LED %d with color 0x%06x", lightedLeds, RGBcolor);
-        pixels->setPixelColor(lightedLeds, RGBcolor);
-        pixels->show();
+      if (isOn() && lightedLeds<numberOfLeds && counter%20==0) {
         lightedLeds++;
       }
-      // LEDs changing colour
-      if (LastColor != RGBcolor) {
-        LastColor = RGBcolor;
-        for (int i=0; i<lightedLeds; i++) {
-          pixels->setPixelColor(i, RGBcolor);
+      // LEDs colour changing
+      for (int i=0; i<lightedLeds; i++) {
+        if (counter*256 > 5*65536) {
+          counter = 0;
         }
-        pixels->show();
+        int pixelHue = 256*counter + (i * 65536L / numberOfLeds);
+        pixels->setPixelColor(i, pixels->gamma32(pixels->ColorHSV(pixelHue)));
       }
+      pixels->show();
       // LEDs switching OFF
-      if (!isOn() && lightedLeds>0 && (millis()-lastTime >= OneLedTime) || (millis() < lastTime)) {
-        lastTime = millis();
+      if (!isOn() && lightedLeds>0 && counter%20==0) {
         lightedLeds--;
-        SUPLA_LOG_DEBUG("RGB strip: switching off LED %d with color 0x%06x", lightedLeds, RGBcolor);
         pixels->setPixelColor(lightedLeds, 0);
         pixels->show();
       }
     }
+  }
 
-    void iterateAlways_Swap() {
-      if (isOn() && ((millis()-lastTime >= OneLedTime) || (millis() < lastTime))) {
-        lastTime = millis();
-        counter++;
-        for (int i=0; i<numberOfLeds; i++) {
-          if ((i+counter)%2) {
-            pixels->setPixelColor(i, RGBcolor);
-          } else {
-            pixels->setPixelColor(i, 0);
-          }
-        }
-        pixels->show();
-      }
-      if (!isOn()) {
-        for (int i=0; i<numberOfLeds; i++) {
-          pixels->setPixelColor(i, 0);
-        }
-        pixels->show();
-      }
+  uint32_t RainbowWheel(byte WheelPos) {
+    if(WheelPos < 85) {
+      return pixels->Color(WheelPos * 3, 255 - WheelPos * 3, 0);
     } 
-
-    void iterateAlways_Flow() {
-      if (isOn() && ((millis()-lastTime >= OneLedTime) || (millis() < lastTime))) {
-        lastTime = millis();
-        counter++;
-        for (int i=0; i<numberOfLeds; i++) {
-          if ((i+counter)%4) {
-            pixels->setPixelColor(i, RGBcolor);
-          } else {
-            pixels->setPixelColor(i, 0);
-          }
-        }
-        pixels->show();
-      }
-      if (!isOn()) {
-        for (int i=0; i<numberOfLeds; i++) {
-          pixels->setPixelColor(i, 0);
-        }
-        pixels->show();
-      }
+    else if(WheelPos < 170) {
+      WheelPos -= 85;
+      return pixels->Color(255 - WheelPos * 3, 0, WheelPos * 3);
     } 
-
-    void iterateAlways_RainbowWheel() {
-      if (isOn() && ((millis()-lastTime >= OneLedTime) || (millis() < lastTime))) {
-        lastTime = millis();
-        counter++;
-        for (int i=0; i<numberOfLeds; i++) {
-          if (counter>255) {
-            counter = 0;
-          }
-          pixels->setPixelColor(i, RainbowWheel((i*1+counter) & 255));
-        }
-        pixels->show();
-      }
-      if (!isOn()) {
-        for (int i=0; i<numberOfLeds; i++) {
-          pixels->setPixelColor(i, 0);
-        }
-        pixels->show();
-      }
-    } 
-
-    void iterateAlways_Rainbow() {
-      if (isOn() && ((millis()-lastTime >= OneLedTime) || (millis() < lastTime))) {
-        lastTime = millis();
-        counter++;
-        for (int i=0; i<lightedLeds; i++) {
-          if (counter*256 > 5*65536) {
-            counter = 0;
-          }
-          int pixelHue = 256*counter + (i * 65536L / numberOfLeds);
-          pixels->setPixelColor(i, pixels->gamma32(pixels->ColorHSV(pixelHue)));
-        }
-        pixels->show();
-      }
-      if (!isOn()) {
-        for (int i=0; i<numberOfLeds; i++) {
-          pixels->setPixelColor(i, 0);
-        }
-        pixels->show();
-      }
-    } 
-
-    void iterateAlways_RainbowVeeroos() {
-      if ((millis()-lastTime >= OneLedTime) || (millis() < lastTime)) {
-        lastTime = millis();
-        counter++;
-
-        // LEDs switching ON
-        if (isOn() && lightedLeds<numberOfLeds && counter%20==0) {
-          lightedLeds++;
-        }
-        // LEDs colour changing
-        for (int i=0; i<lightedLeds; i++) {
-          if (counter*256 > 5*65536) {
-            counter = 0;
-          }
-          int pixelHue = 256*counter + (i * 65536L / numberOfLeds);
-          pixels->setPixelColor(i, pixels->gamma32(pixels->ColorHSV(pixelHue)));
-        }
-        pixels->show();
-        // LEDs switching OFF
-        if (!isOn() && lightedLeds>0 && counter%20==0) {
-          lightedLeds--;
-          pixels->setPixelColor(lightedLeds, 0);
-          pixels->show();
-        }
-      }
+    else {
+      WheelPos -= 170;
+      return pixels->Color(0, WheelPos * 3, 255 - WheelPos * 3);
     }
-
-    uint32_t RainbowWheel(byte WheelPos) {
-      if(WheelPos < 85) {
-        return pixels->Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-      } 
-      else if(WheelPos < 170) {
-        WheelPos -= 85;
-        return pixels->Color(255 - WheelPos * 3, 0, WheelPos * 3);
-      } 
-      else {
-        WheelPos -= 170;
-        return pixels->Color(0, WheelPos * 3, 255 - WheelPos * 3);
-      }
-    }
+  }
 };
 
 }  // namespace Control

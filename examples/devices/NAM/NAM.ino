2ae4d6e36fa32017044800495b27068233f9a936
malarz <malarz@batory.edu.pl>
2026-01-19 21:50:02 +0100

    Update light sensor caption to English

--- examples/devices/NAM/NAM.ino
+++ examples/devices/NAM/NAM.ino
@@ -1,3 +1,21 @@
+/*
+   Copyright (C) malarz
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+   */
+
 #define NAM_VERSION "0.5"
 #define DEV_NAME "NAM 0.3.3"
 
@@ -81,7 +99,7 @@ void setup() {
 
   // Rohm Semiconductor BH1750FVI
   auto bh1750 = new Supla::Sensor::Bh1750(0x23);
-  bh1750->setInitialCaption("Czujnik światła");
+  bh1750->setInitialCaption("Light sensor");
 
   // NovaFitness SDS011
   auto sds011 = new Supla::Sensor::ParticleMeterSDS011(SDS_RX_PIN, SDS_TX_PIN, 1);

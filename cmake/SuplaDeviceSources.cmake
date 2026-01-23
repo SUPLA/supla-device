ac692f0328f49f1303b71493f12df500f39c8937
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2026-01-23 11:50:25 +0100

    Add missing cpp file to cmake

--- cmake/SuplaDeviceSources.cmake
+++ cmake/SuplaDeviceSources.cmake
@@ -116,6 +116,7 @@ set(SUPLA_DEVICE_SRCS
   ${SUPLA_DEVICE_SRC_DIR}/supla/network/html/div.cpp
   ${SUPLA_DEVICE_SRC_DIR}/supla/network/html/hide_show_container.cpp
   ${SUPLA_DEVICE_SRC_DIR}/supla/network/html/channel_correction.cpp
+  ${SUPLA_DEVICE_SRC_DIR}/supla/network/html/hvac_parameters.cpp
 
   ${SUPLA_DEVICE_SRC_DIR}/supla/network/client.cpp
   ${SUPLA_DEVICE_SRC_DIR}/supla/network/ip_address.cpp

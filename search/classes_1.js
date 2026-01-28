01d3bcd8ef9e3acc0bc8b232b81f80247cc7c4bd
klew <klew@users.noreply.github.com>
2026-01-28 13:30:50 +0000

    deploy: 1fa8a06df70b6bc0a4b1b5d0c374b5fcbbf559f6

--- search/classes_1.js
+++ search/classes_1.js
@@ -3,10 +3,11 @@ var searchData=
   ['actionhandler_0',['ActionHandler',['../classSupla_1_1ActionHandler.html',1,'Supla']]],
   ['actionhandlerclient_1',['ActionHandlerClient',['../classSupla_1_1ActionHandlerClient.html',1,'Supla']]],
   ['actiontrigger_2',['ActionTrigger',['../classSupla_1_1Control_1_1ActionTrigger.html',1,'Supla::Control']]],
-  ['ads1115_3',['ADS1115',['../classSupla_1_1Io_1_1ADS1115.html',1,'Supla::Io']]],
-  ['afore_4',['Afore',['../classSupla_1_1PV_1_1Afore.html',1,'Supla::PV']]],
-  ['aht_5',['AHT',['../classSupla_1_1Sensor_1_1AHT.html',1,'Supla::Sensor']]],
-  ['aqieco_6',['AQIECO',['../classSupla_1_1Protocol_1_1AQIECO.html',1,'Supla::Protocol']]],
-  ['atchannel_7',['AtChannel',['../classSupla_1_1AtChannel.html',1,'Supla']]],
-  ['autolock_8',['AutoLock',['../classSupla_1_1AutoLock.html',1,'Supla']]]
+  ['addressableleds_3',['AddressableLEDs',['../classSupla_1_1Control_1_1AddressableLEDs.html',1,'Supla::Control']]],
+  ['ads1115_4',['ADS1115',['../classSupla_1_1Io_1_1ADS1115.html',1,'Supla::Io']]],
+  ['afore_5',['Afore',['../classSupla_1_1PV_1_1Afore.html',1,'Supla::PV']]],
+  ['aht_6',['AHT',['../classSupla_1_1Sensor_1_1AHT.html',1,'Supla::Sensor']]],
+  ['aqieco_7',['AQIECO',['../classSupla_1_1Protocol_1_1AQIECO.html',1,'Supla::Protocol']]],
+  ['atchannel_8',['AtChannel',['../classSupla_1_1AtChannel.html',1,'Supla']]],
+  ['autolock_9',['AutoLock',['../classSupla_1_1AutoLock.html',1,'Supla']]]
 ];

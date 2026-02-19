537941510234533f1d018f4fdec5eb3e33945edd
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2026-02-19 11:04:54 +0100

    sd4linux: Add parsing an attempt to parse string value as double (i.e.               if it contain "45 percent" -> 45.0). Add additional checks to clarify missing json key vs invalid data value errors

--- extras/porting/linux/supla/parser/json.cpp
+++ extras/porting/linux/supla/parser/json.cpp
@@ -17,8 +17,9 @@
 #include "json.h"
 
 #include <supla/log_wrapper.h>
-#include <string>
+
 #include <stdexcept>
+#include <string>
 
 Supla::Parser::Json::Json(Supla::Source::Source* src)
     : Supla::Parser::Parser(src) {
@@ -55,19 +56,58 @@ bool Supla::Parser::Json::isValid() {
 
 double Supla::Parser::Json::getValue(const std::string& key) {
   try {
-    if (key[0] == '/') {
-      return json[nlohmann::json::json_pointer(key)].get<double>();
+    const nlohmann::json* valuePtr = nullptr;
+
+    if (!key.empty() && key[0] == '/') {
+      nlohmann::json::json_pointer ptr(key);
+
+      if (!json.contains(ptr)) {
+        SUPLA_LOG_ERROR("JSON key \"%s\" not found", key.c_str());
+        valid = false;
+        return 0;
+      }
+
+      valuePtr = &json.at(ptr);
     } else {
-      return json[key].get<double>();
+      if (!json.contains(key)) {
+        SUPLA_LOG_ERROR("JSON key \"%s\" not found", key.c_str());
+        valid = false;
+        return 0;
+      }
+
+      valuePtr = &json.at(key);
     }
-  }
-  catch (nlohmann::json::type_error& ex) {
+
+    if (valuePtr->is_number()) {
+      return valuePtr->get<double>();
+    }
+
+    if (valuePtr->is_string()) {
+      // Try to convert string to double
+      try {
+        return std::stod(valuePtr->get<std::string>());
+      } catch (...) {
+        SUPLA_LOG_ERROR("JSON key \"%s\" string cannot be converted to double",
+                        key.c_str());
+        valid = false;
+        return 0;
+      }
+    }
+
+    SUPLA_LOG_ERROR("JSON key \"%s\" has invalid data type (double expected)",
+                    key.c_str());
+    valid = false;
+    return 0;
+  } catch (const nlohmann::json::out_of_range&) {
     SUPLA_LOG_ERROR("JSON key \"%s\" not found", key.c_str());
     valid = false;
-  }
-  catch (...) {
-    SUPLA_LOG_ERROR(
-        "JSON exception during getValue for key \"%s\"", key.c_str());
+  } catch (const nlohmann::json::type_error&) {
+    SUPLA_LOG_ERROR("JSON key \"%s\" has invalid data type (double expected)",
+                    key.c_str());
+    valid = false;
+  } catch (const std::exception& ex) {
+    SUPLA_LOG_ERROR("JSON exception during getValue for key \"%s\": %s",
+                    key.c_str(), ex.what());
     valid = false;
   }
 
@@ -82,41 +122,35 @@ std::variant<int, bool, std::string> Supla::Parser::Json::getStateValue(
     } else {
       return json[key].get<bool>();
     }
-  }
-  catch (nlohmann::json::type_error& ex) {
+  } catch (nlohmann::json::type_error& ex) {
     try {
       if (key[0] == '/') {
         return json[nlohmann::json::json_pointer(key)].get<int>();
       } else {
         return json[key].get<int>();
       }
-    }
-    catch (nlohmann::json::type_error& ex) {
+    } catch (nlohmann::json::type_error& ex) {
       try {
         if (key[0] == '/') {
           return json[nlohmann::json::json_pointer(key)].get<std::string>();
         } else {
           return json[key].get<std::string>();
         }
-      }
-      catch (nlohmann::json::type_error& ex) {
+      } catch (nlohmann::json::type_error& ex) {
+        valid = false;
+      } catch (...) {
+        SUPLA_LOG_ERROR("JSON exception during getStateValue for key \"%s\"",
+                        key.c_str());
         valid = false;
       }
-      catch (...) {
-        SUPLA_LOG_ERROR(
-            "JSON exception during getStateValue for key \"%s\"", key.c_str());
-        valid = false;
-      }
-    }
-    catch (...) {
-      SUPLA_LOG_ERROR(
-          "JSON exception during getStateValue for key \"%s\"", key.c_str());
+    } catch (...) {
+      SUPLA_LOG_ERROR("JSON exception during getStateValue for key \"%s\"",
+                      key.c_str());
       valid = false;
     }
-  }
-  catch (...) {
-    SUPLA_LOG_ERROR(
-        "JSON exception during getStateValue for key \"%s\"", key.c_str());
+  } catch (...) {
+    SUPLA_LOG_ERROR("JSON exception during getStateValue for key \"%s\"",
+                    key.c_str());
     valid = false;
   }
   return 0;

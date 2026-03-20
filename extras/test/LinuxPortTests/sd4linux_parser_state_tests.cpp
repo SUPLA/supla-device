31c99136b02c32f15a0701c54d4692474b1406a6
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2026-03-20 14:57:42 +0100

    sd4linux: add some tests

--- extras/test/LinuxPortTests/sd4linux_parser_state_tests.cpp
+++ extras/test/LinuxPortTests/sd4linux_parser_state_tests.cpp
@@ -144,3 +144,152 @@ TEST_F(Sd4linuxParserStateTests,
   EXPECT_FALSE(sensor.getChannel()->isStateOnline());
   EXPECT_EQ(parser.refreshCount, 1);
 }
+
+TEST_F(Sd4linuxParserStateTests, ReturnsChannelOnlineAfterSourceReconnects) {
+  SimpleTime time;
+  FakeSd4linuxSource source;
+  FakeSd4linuxParser parser(&source);
+  parser.setRefreshTime(5000);
+
+  Supla::Sensor::BinaryParsed sensor(&parser);
+  sensor.setMapping(Supla::Parser::State, "state");
+  sensor.setUseOfflineOnInvalidState(true);
+
+  time.advance(101);
+  sensor.iterateAlways();
+
+  ASSERT_TRUE(sensor.getChannel()->isStateOnline());
+  ASSERT_EQ(parser.refreshCount, 1);
+
+  source.connected = false;
+  time.advance(101);
+  sensor.iterateAlways();
+
+  ASSERT_FALSE(sensor.getChannel()->isStateOnline());
+  ASSERT_EQ(parser.refreshCount, 1);
+
+  source.connected = true;
+  sensor.getChannel()->clearSendValue();
+
+  time.advance(5001);
+  sensor.iterateAlways();
+
+  EXPECT_TRUE(sensor.getChannel()->isStateOnline());
+  EXPECT_TRUE(sensor.getChannel()->isUpdateReady());
+  EXPECT_EQ(parser.refreshCount, 2);
+}
+
+TEST_F(Sd4linuxParserStateTests,
+       ReturnsChannelOnlineAfterParserDataBecomesValidAgain) {
+  SimpleTime time;
+  FakeSd4linuxSource source;
+  FakeSd4linuxParser parser(&source);
+  parser.setRefreshTime(5000);
+
+  Supla::Sensor::BinaryParsed sensor(&parser);
+  sensor.setMapping(Supla::Parser::State, "state");
+  sensor.setUseOfflineOnInvalidState(true);
+
+  time.advance(101);
+  sensor.iterateAlways();
+
+  ASSERT_TRUE(sensor.getChannel()->isStateOnline());
+  ASSERT_EQ(parser.refreshCount, 1);
+
+  parser.validAfterRefresh = false;
+  time.advance(5001);
+  sensor.iterateAlways();
+
+  ASSERT_FALSE(sensor.getChannel()->isStateOnline());
+  ASSERT_EQ(parser.refreshCount, 2);
+
+  parser.validAfterRefresh = true;
+  sensor.getChannel()->clearSendValue();
+
+  time.advance(5001);
+  sensor.iterateAlways();
+
+  EXPECT_TRUE(sensor.getChannel()->isStateOnline());
+  EXPECT_TRUE(sensor.getChannel()->isUpdateReady());
+  EXPECT_EQ(parser.refreshCount, 3);
+}
+
+TEST_F(Sd4linuxParserStateTests,
+       KeepsChannelOnlineWhenInvalidStateDoesNotMeanOffline) {
+  SimpleTime time;
+  FakeSd4linuxSource source;
+  FakeSd4linuxParser parser(&source);
+  parser.setRefreshTime(5000);
+
+  Supla::Sensor::BinaryParsed sensor(&parser);
+  sensor.setMapping(Supla::Parser::State, "state");
+
+  time.advance(101);
+  sensor.iterateAlways();
+
+  ASSERT_TRUE(sensor.getChannel()->isStateOnline());
+
+  parser.validAfterRefresh = false;
+  time.advance(5001);
+  sensor.iterateAlways();
+
+  EXPECT_TRUE(sensor.getChannel()->isStateOnline());
+  EXPECT_EQ(parser.refreshCount, 2);
+}
+
+TEST_F(Sd4linuxParserStateTests, ReadsBinaryStateFromBoolValues) {
+  SimpleTime time;
+  FakeSd4linuxSource source;
+  FakeSd4linuxParser parser(&source);
+  Supla::Sensor::BinaryParsed sensor(&parser);
+  sensor.setMapping(Supla::Parser::State, "state");
+
+  parser.stateValue = true;
+  sensor.onInit();
+  EXPECT_TRUE(sensor.getValue());
+  EXPECT_TRUE(sensor.getChannel()->getValueBool());
+
+  parser.stateValue = false;
+  time.advance(101);
+  sensor.iterateAlways();
+  EXPECT_FALSE(sensor.getValue());
+  EXPECT_FALSE(sensor.getChannel()->getValueBool());
+}
+
+TEST_F(Sd4linuxParserStateTests, ReadsBinaryStateFromNumericValues) {
+  SimpleTime time;
+  FakeSd4linuxSource source;
+  FakeSd4linuxParser parser(&source);
+  Supla::Sensor::BinaryParsed sensor(&parser);
+  sensor.setMapping(Supla::Parser::State, "state");
+
+  parser.stateValue = 1;
+  sensor.onInit();
+  EXPECT_TRUE(sensor.getValue());
+  EXPECT_TRUE(sensor.getChannel()->getValueBool());
+
+  parser.stateValue = 0;
+  time.advance(101);
+  sensor.iterateAlways();
+  EXPECT_FALSE(sensor.getValue());
+  EXPECT_FALSE(sensor.getChannel()->getValueBool());
+}
+
+TEST_F(Sd4linuxParserStateTests, ReadsBinaryStateFromTextValues) {
+  SimpleTime time;
+  FakeSd4linuxSource source;
+  FakeSd4linuxParser parser(&source);
+  Supla::Sensor::BinaryParsed sensor(&parser);
+  sensor.setMapping(Supla::Parser::State, "state");
+
+  parser.stateValue = std::string("ON");
+  sensor.onInit();
+  EXPECT_TRUE(sensor.getValue());
+  EXPECT_TRUE(sensor.getChannel()->getValueBool());
+
+  parser.stateValue = std::string("no");
+  time.advance(101);
+  sensor.iterateAlways();
+  EXPECT_FALSE(sensor.getValue());
+  EXPECT_FALSE(sensor.getChannel()->getValueBool());
+}

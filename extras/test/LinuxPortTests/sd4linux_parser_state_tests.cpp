e8a9a7ac1e56818b8527797001d0e57362a69f0b
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2026-03-20 13:48:40 +0100

    sd4linux: add first tests

---
+++ extras/test/LinuxPortTests/sd4linux_parser_state_tests.cpp
@@ -0,0 +1,146 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include <gtest/gtest.h>
+#include <simple_time.h>
+#include <supla/parser/parser.h>
+#include <supla/sensor/binary_parsed.h>
+#include <supla/source/source.h>
+
+#include <string>
+#include <variant>
+
+namespace {
+
+class FakeSd4linuxSource : public Supla::Source::Source {
+ public:
+  bool connected = true;
+
+  std::string getContent() override {
+    return {};
+  }
+
+  bool isConnected() override {
+    return connected;
+  }
+};
+
+class FakeSd4linuxParser : public Supla::Parser::Parser {
+ public:
+  explicit FakeSd4linuxParser(Supla::Source::Source *source)
+      : Supla::Parser::Parser(source) {
+  }
+
+  bool validAfterRefresh = true;
+  std::variant<int, bool, std::string> stateValue = 1;
+  int refreshCount = 0;
+
+  double getValue(const std::string &) override {
+    return 0;
+  }
+
+  std::variant<int, bool, std::string> getStateValue(
+      const std::string &) override {
+    return stateValue;
+  }
+
+  bool isBasedOnIndex() override {
+    return false;
+  }
+
+ protected:
+  bool refreshSource() override {
+    refreshCount++;
+    valid = validAfterRefresh;
+    return valid;
+  }
+};
+
+class Sd4linuxParserStateTests : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    Supla::Channel::resetToDefaults();
+  }
+
+  void TearDown() override {
+    Supla::Channel::resetToDefaults();
+  }
+};
+
+}  // namespace
+
+TEST_F(Sd4linuxParserStateTests,
+       KeepsChannelOfflineWhenParserIsStillInvalidDuringCacheHit) {
+  SimpleTime time;
+  FakeSd4linuxSource source;
+  FakeSd4linuxParser parser(&source);
+  parser.setRefreshTime(5000);
+
+  Supla::Sensor::BinaryParsed sensor(&parser);
+  sensor.setMapping(Supla::Parser::State, "state");
+  sensor.setUseOfflineOnInvalidState(true);
+
+  time.advance(101);
+  sensor.iterateAlways();
+
+  EXPECT_TRUE(sensor.getChannel()->isStateOnline());
+  EXPECT_EQ(parser.refreshCount, 1);
+
+  parser.validAfterRefresh = false;
+
+  time.advance(5001);
+  sensor.iterateAlways();
+
+  EXPECT_FALSE(sensor.getChannel()->isStateOnline());
+  EXPECT_EQ(parser.refreshCount, 2);
+
+  sensor.getChannel()->clearSendValue();
+  EXPECT_FALSE(sensor.getChannel()->isUpdateReady());
+
+  time.advance(101);
+  sensor.iterateAlways();
+
+  EXPECT_FALSE(sensor.getChannel()->isStateOnline());
+  EXPECT_EQ(parser.refreshCount, 2);
+  EXPECT_FALSE(sensor.getChannel()->isUpdateReady());
+}
+
+TEST_F(Sd4linuxParserStateTests,
+       MarksChannelOfflineImmediatelyWhenSourceDrops) {
+  SimpleTime time;
+  FakeSd4linuxSource source;
+  FakeSd4linuxParser parser(&source);
+  parser.setRefreshTime(5000);
+
+  Supla::Sensor::BinaryParsed sensor(&parser);
+  sensor.setMapping(Supla::Parser::State, "state");
+  sensor.setUseOfflineOnInvalidState(true);
+
+  time.advance(101);
+  sensor.iterateAlways();
+
+  ASSERT_TRUE(sensor.getChannel()->isStateOnline());
+
+  source.connected = false;
+
+  time.advance(101);
+  sensor.iterateAlways();
+
+  EXPECT_FALSE(sensor.getChannel()->isStateOnline());
+  EXPECT_EQ(parser.refreshCount, 1);
+}

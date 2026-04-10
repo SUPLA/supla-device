2a446caf794cbd86c7c9ffc6b3f77a75f4cd5941
klew <klew@users.noreply.github.com>
2026-04-10 11:52:22 +0000

    deploy: 44e757322a58390dd0de772a92811d89b48db7c3

--- search/functions_10.js
+++ search/functions_10.js
@@ -3,5 +3,6 @@ var searchData=
   ['tag_0',['tag',['../classSupla_1_1WebSender.html#ab09f9c3a1203002f8ddbf1c6a2fe5124',1,'Supla::WebSender']]],
   ['teststepstatusled_1',['testStepStatusLed',['../classSuplaDeviceClass.html#a3a40abe12efe6ffbb2f350270c7d789e',1,'SuplaDeviceClass']]],
   ['textinput_2',['textInput',['../classSupla_1_1WebSender.html#adc3259e1fa292015b378bbcfd6983423',1,'Supla::WebSender']]],
-  ['togglebox_3',['toggleBox',['../classSupla_1_1WebSender.html#aa010db72536045b3791b7c3a5ce00a1d',1,'Supla::WebSender']]]
+  ['toggle_3',['toggle',['../classSupla_1_1Sensor_1_1VirtualBinary.html#a852b5077163802e7193c18635c2a8d41',1,'Supla::Sensor::VirtualBinary']]],
+  ['togglebox_4',['toggleBox',['../classSupla_1_1WebSender.html#aa010db72536045b3791b7c3a5ce00a1d',1,'Supla::WebSender']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -2,11 +2,12 @@ var searchData=
 [
   ['checkboxinput_0',['checkboxInput',['../classSupla_1_1WebSender.html#ac3c1cde4a6b0c16671032da2584ec63a',1,'Supla::WebSender']]],
   ['checksensorinvalidstate_1',['checkSensorInvalidState',['../classSupla_1_1Sensor_1_1Container.html#ad993ee738a1524a8b6d248038c4af678',1,'Supla::Sensor::Container::checkSensorInvalidState()'],['../classContainer.html#ad993ee738a1524a8b6d248038c4af678',1,'Container::checkSensorInvalidState()']]],
-  ['clearcurrentphasesequenceflag_2',['clearCurrentPhaseSequenceFlag',['../classSupla_1_1Sensor_1_1ElectricityMeter.html#a41e1ea07a89e920dab8d1b5969758674',1,'Supla::Sensor::ElectricityMeter::clearCurrentPhaseSequenceFlag()'],['../classElectricityMeter.html#a41e1ea07a89e920dab8d1b5969758674',1,'ElectricityMeter::clearCurrentPhaseSequenceFlag()']]],
-  ['clearinvalidptr_3',['ClearInvalidPtr',['../classSupla_1_1Element.html#a59b914551b75cc301fb47dfc2c9f4115',1,'Supla::Element']]],
-  ['clearvoltagephasesequenceflag_4',['clearVoltagePhaseSequenceFlag',['../classSupla_1_1Sensor_1_1ElectricityMeter.html#ac0b73024e76a885d2869ed1001d29159',1,'Supla::Sensor::ElectricityMeter::clearVoltagePhaseSequenceFlag()'],['../classElectricityMeter.html#ac0b73024e76a885d2869ed1001d29159',1,'ElectricityMeter::clearVoltagePhaseSequenceFlag()']]],
-  ['close_5',['close',['../classSupla_1_1HtmlTag.html#a4723e2d57fa1a70b7cad6092c6368032',1,'Supla::HtmlTag']]],
-  ['closevalve_6',['closeValve',['../classSupla_1_1Control_1_1ValveBase.html#a016316e194423fd6566466e99ce2c5a6',1,'Supla::Control::ValveBase::closeValve()'],['../classValveBase.html#a016316e194423fd6566466e99ce2c5a6',1,'ValveBase::closeValve()']]],
-  ['convertstoragefromlegacychannel_7',['convertStorageFromLegacyChannel',['../classSupla_1_1Control_1_1LightingPwmBase.html#a61e8b252f9d73a5b0ed35e3068cc2985',1,'Supla::Control::LightingPwmBase']]],
-  ['customtextparameter_8',['CustomTextParameter',['../classSupla_1_1Html_1_1CustomTextParameter.html#af91fc4f710d12371213d779804cd2928',1,'Supla::Html::CustomTextParameter']]]
+  ['clear_2',['clear',['../classSupla_1_1Sensor_1_1VirtualBinary.html#a3ec4fe20cd8622646cb01630ee5a9c48',1,'Supla::Sensor::VirtualBinary']]],
+  ['clearcurrentphasesequenceflag_3',['clearCurrentPhaseSequenceFlag',['../classSupla_1_1Sensor_1_1ElectricityMeter.html#a41e1ea07a89e920dab8d1b5969758674',1,'Supla::Sensor::ElectricityMeter::clearCurrentPhaseSequenceFlag()'],['../classElectricityMeter.html#a41e1ea07a89e920dab8d1b5969758674',1,'ElectricityMeter::clearCurrentPhaseSequenceFlag()']]],
+  ['clearinvalidptr_4',['ClearInvalidPtr',['../classSupla_1_1Element.html#a59b914551b75cc301fb47dfc2c9f4115',1,'Supla::Element']]],
+  ['clearvoltagephasesequenceflag_5',['clearVoltagePhaseSequenceFlag',['../classSupla_1_1Sensor_1_1ElectricityMeter.html#ac0b73024e76a885d2869ed1001d29159',1,'Supla::Sensor::ElectricityMeter::clearVoltagePhaseSequenceFlag()'],['../classElectricityMeter.html#ac0b73024e76a885d2869ed1001d29159',1,'ElectricityMeter::clearVoltagePhaseSequenceFlag()']]],
+  ['close_6',['close',['../classSupla_1_1HtmlTag.html#a4723e2d57fa1a70b7cad6092c6368032',1,'Supla::HtmlTag']]],
+  ['closevalve_7',['closeValve',['../classSupla_1_1Control_1_1ValveBase.html#a016316e194423fd6566466e99ce2c5a6',1,'Supla::Control::ValveBase::closeValve()'],['../classValveBase.html#a016316e194423fd6566466e99ce2c5a6',1,'ValveBase::closeValve()']]],
+  ['convertstoragefromlegacychannel_8',['convertStorageFromLegacyChannel',['../classSupla_1_1Control_1_1LightingPwmBase.html#a61e8b252f9d73a5b0ed35e3068cc2985',1,'Supla::Control::LightingPwmBase']]],
+  ['customtextparameter_9',['CustomTextParameter',['../classSupla_1_1Html_1_1CustomTextParameter.html#af91fc4f710d12371213d779804cd2928',1,'Supla::Html::CustomTextParameter']]]
 ];

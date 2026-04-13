c25f01651cf938d0542a38c7acc47100fdb4632c
v33r005 <125038833+v33r005@users.noreply.github.com>
2026-04-13 08:13:39 +0200

    Update ntc10k.cpp

--- src/supla/sensor/ntc10k.cpp
+++ src/supla/sensor/ntc10k.cpp
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
- #include <math.h>
+#include <math.h>
 #include "Arduino.h"
 #include <supla/log_wrapper.h>
 #include <supla/time.h>

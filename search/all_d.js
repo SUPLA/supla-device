8e9357947a87fd14fee6d322349b791af71898e4
klew <klew@users.noreply.github.com>
2026-05-07 10:54:13 +0000

    deploy: e0b6a32d2ad033738352004a3cab20245f0d7f6c

--- search/all_d.js
+++ search/all_d.js
@@ -3,10 +3,11 @@ var searchData=
   ['network_0',['Network',['../classSupla_1_1Network.html',1,'Supla']]],
   ['networkconfig_1',['NetworkConfig',['../structSupla_1_1Modbus_1_1NetworkConfig.html',1,'Supla::Modbus']]],
   ['next_2',['next',['../classSupla_1_1Element.html#ab9d7ce67058db644bbdf3e6e8dac91c4',1,'Supla::Element']]],
-  ['normallyopen_3',['NormallyOpen',['../classSupla_1_1Sensor_1_1NormallyOpen.html',1,'Supla::Sensor']]],
-  ['notification_4',['Notification',['../classNotification.html',1,'Notification'],['../classSupla_1_1Notification.html',1,'Supla::Notification']]],
-  ['notifyelementsaboutconfigchange_5',['NotifyElementsAboutConfigChange',['../classSupla_1_1Element.html#ad7e52c7c63e007e33b840e85d90c2f78',1,'Supla::Element']]],
-  ['ntc10k_6',['NTC10k',['../classSupla_1_1Sensor_1_1NTC10k.html',1,'Supla::Sensor']]],
-  ['numberinput_7',['numberInput',['../classSupla_1_1WebSender.html#ac65930248039f07554f96ac3c6723b75',1,'Supla::WebSender::numberInput(const char *key, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)'],['../classSupla_1_1WebSender.html#afbea032e2194b4aed5721ed31b1b2b99',1,'Supla::WebSender::numberInput(const char *name, const char *id, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)']]],
-  ['numericinputspec_8',['NumericInputSpec',['../structSupla_1_1NumericInputSpec.html',1,'Supla']]]
+  ['normalizepwmfrequency_3',['normalizePwmFrequency',['../classSupla_1_1Control_1_1LightingPwmBase.html#a539b95bbd9135cbe236bc489acfbf842',1,'Supla::Control::LightingPwmBase']]],
+  ['normallyopen_4',['NormallyOpen',['../classSupla_1_1Sensor_1_1NormallyOpen.html',1,'Supla::Sensor']]],
+  ['notification_5',['Notification',['../classNotification.html',1,'Notification'],['../classSupla_1_1Notification.html',1,'Supla::Notification']]],
+  ['notifyelementsaboutconfigchange_6',['NotifyElementsAboutConfigChange',['../classSupla_1_1Element.html#ad7e52c7c63e007e33b840e85d90c2f78',1,'Supla::Element']]],
+  ['ntc10k_7',['NTC10k',['../classSupla_1_1Sensor_1_1NTC10k.html',1,'Supla::Sensor']]],
+  ['numberinput_8',['numberInput',['../classSupla_1_1WebSender.html#ac65930248039f07554f96ac3c6723b75',1,'Supla::WebSender::numberInput(const char *key, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)'],['../classSupla_1_1WebSender.html#afbea032e2194b4aed5721ed31b1b2b99',1,'Supla::WebSender::numberInput(const char *name, const char *id, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)']]],
+  ['numericinputspec_9',['NumericInputSpec',['../structSupla_1_1NumericInputSpec.html',1,'Supla']]]
 ];
--- search/functions_b.js
+++ search/functions_b.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['next_0',['next',['../classSupla_1_1Element.html#ab9d7ce67058db644bbdf3e6e8dac91c4',1,'Supla::Element']]],
-  ['notifyelementsaboutconfigchange_1',['NotifyElementsAboutConfigChange',['../classSupla_1_1Element.html#ad7e52c7c63e007e33b840e85d90c2f78',1,'Supla::Element']]],
-  ['numberinput_2',['numberInput',['../classSupla_1_1WebSender.html#ac65930248039f07554f96ac3c6723b75',1,'Supla::WebSender::numberInput(const char *key, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)'],['../classSupla_1_1WebSender.html#afbea032e2194b4aed5721ed31b1b2b99',1,'Supla::WebSender::numberInput(const char *name, const char *id, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)']]]
+  ['normalizepwmfrequency_1',['normalizePwmFrequency',['../classSupla_1_1Control_1_1LightingPwmBase.html#a539b95bbd9135cbe236bc489acfbf842',1,'Supla::Control::LightingPwmBase']]],
+  ['notifyelementsaboutconfigchange_2',['NotifyElementsAboutConfigChange',['../classSupla_1_1Element.html#ad7e52c7c63e007e33b840e85d90c2f78',1,'Supla::Element']]],
+  ['numberinput_3',['numberInput',['../classSupla_1_1WebSender.html#ac65930248039f07554f96ac3c6723b75',1,'Supla::WebSender::numberInput(const char *key, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)'],['../classSupla_1_1WebSender.html#afbea032e2194b4aed5721ed31b1b2b99',1,'Supla::WebSender::numberInput(const char *name, const char *id, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)']]]
 ];

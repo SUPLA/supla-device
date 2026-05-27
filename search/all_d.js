8bc87818e96940a3790a3dea1b360987adab0f69
klew <klew@users.noreply.github.com>
2026-05-27 06:52:26 +0000

    deploy: 33725e4a2cf25e0740a69086bbeba565f4374c29

--- search/all_d.js
+++ search/all_d.js
@@ -1,13 +1,15 @@
 var searchData=
 [
-  ['network_0',['Network',['../classSupla_1_1Network.html',1,'Supla']]],
-  ['networkconfig_1',['NetworkConfig',['../structSupla_1_1Modbus_1_1NetworkConfig.html',1,'Supla::Modbus']]],
-  ['next_2',['next',['../classSupla_1_1Element.html#ab9d7ce67058db644bbdf3e6e8dac91c4',1,'Supla::Element']]],
-  ['normalizepwmfrequency_3',['normalizePwmFrequency',['../classSupla_1_1Control_1_1LightingPwmBase.html#a539b95bbd9135cbe236bc489acfbf842',1,'Supla::Control::LightingPwmBase']]],
-  ['normallyopen_4',['NormallyOpen',['../classSupla_1_1Sensor_1_1NormallyOpen.html',1,'Supla::Sensor']]],
-  ['notification_5',['Notification',['../classNotification.html',1,'Notification'],['../classSupla_1_1Notification.html',1,'Supla::Notification']]],
-  ['notifyelementsaboutconfigchange_6',['NotifyElementsAboutConfigChange',['../classSupla_1_1Element.html#ad7e52c7c63e007e33b840e85d90c2f78',1,'Supla::Element']]],
-  ['ntc10k_7',['NTC10k',['../classSupla_1_1Sensor_1_1NTC10k.html',1,'Supla::Sensor']]],
-  ['numberinput_8',['numberInput',['../classSupla_1_1WebSender.html#ac65930248039f07554f96ac3c6723b75',1,'Supla::WebSender::numberInput(const char *key, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)'],['../classSupla_1_1WebSender.html#afbea032e2194b4aed5721ed31b1b2b99',1,'Supla::WebSender::numberInput(const char *name, const char *id, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)']]],
-  ['numericinputspec_9',['NumericInputSpec',['../structSupla_1_1NumericInputSpec.html',1,'Supla']]]
+  ['netifconfigblob_0',['NetifConfigBlob',['../structSupla_1_1NetifConfigBlob.html',1,'Supla']]],
+  ['network_1',['Network',['../classSupla_1_1Network.html',1,'Supla']]],
+  ['networkaddressparameters_2',['NetworkAddressParameters',['../classSupla_1_1Html_1_1NetworkAddressParameters.html',1,'Supla::Html']]],
+  ['networkconfig_3',['NetworkConfig',['../structSupla_1_1Modbus_1_1NetworkConfig.html',1,'Supla::Modbus']]],
+  ['next_4',['next',['../classSupla_1_1Element.html#ab9d7ce67058db644bbdf3e6e8dac91c4',1,'Supla::Element']]],
+  ['normalizepwmfrequency_5',['normalizePwmFrequency',['../classSupla_1_1Control_1_1LightingPwmBase.html#a539b95bbd9135cbe236bc489acfbf842',1,'Supla::Control::LightingPwmBase']]],
+  ['normallyopen_6',['NormallyOpen',['../classSupla_1_1Sensor_1_1NormallyOpen.html',1,'Supla::Sensor']]],
+  ['notification_7',['Notification',['../classNotification.html',1,'Notification'],['../classSupla_1_1Notification.html',1,'Supla::Notification']]],
+  ['notifyelementsaboutconfigchange_8',['NotifyElementsAboutConfigChange',['../classSupla_1_1Element.html#ad7e52c7c63e007e33b840e85d90c2f78',1,'Supla::Element']]],
+  ['ntc10k_9',['NTC10k',['../classSupla_1_1Sensor_1_1NTC10k.html',1,'Supla::Sensor']]],
+  ['numberinput_10',['numberInput',['../classSupla_1_1WebSender.html#ac65930248039f07554f96ac3c6723b75',1,'Supla::WebSender::numberInput(const char *key, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)'],['../classSupla_1_1WebSender.html#afbea032e2194b4aed5721ed31b1b2b99',1,'Supla::WebSender::numberInput(const char *name, const char *id, const NumericInputSpec &amp;spec, const char *cssClass=nullptr)']]],
+  ['numericinputspec_11',['NumericInputSpec',['../structSupla_1_1NumericInputSpec.html',1,'Supla']]]
 ];
--- search/classes_d.js
+++ search/classes_d.js
@@ -1,9 +1,11 @@
 var searchData=
 [
-  ['network_0',['Network',['../classSupla_1_1Network.html',1,'Supla']]],
-  ['networkconfig_1',['NetworkConfig',['../structSupla_1_1Modbus_1_1NetworkConfig.html',1,'Supla::Modbus']]],
-  ['normallyopen_2',['NormallyOpen',['../classSupla_1_1Sensor_1_1NormallyOpen.html',1,'Supla::Sensor']]],
-  ['notification_3',['Notification',['../classNotification.html',1,'Notification'],['../classSupla_1_1Notification.html',1,'Supla::Notification']]],
-  ['ntc10k_4',['NTC10k',['../classSupla_1_1Sensor_1_1NTC10k.html',1,'Supla::Sensor']]],
-  ['numericinputspec_5',['NumericInputSpec',['../structSupla_1_1NumericInputSpec.html',1,'Supla']]]
+  ['netifconfigblob_0',['NetifConfigBlob',['../structSupla_1_1NetifConfigBlob.html',1,'Supla']]],
+  ['network_1',['Network',['../classSupla_1_1Network.html',1,'Supla']]],
+  ['networkaddressparameters_2',['NetworkAddressParameters',['../classSupla_1_1Html_1_1NetworkAddressParameters.html',1,'Supla::Html']]],
+  ['networkconfig_3',['NetworkConfig',['../structSupla_1_1Modbus_1_1NetworkConfig.html',1,'Supla::Modbus']]],
+  ['normallyopen_4',['NormallyOpen',['../classSupla_1_1Sensor_1_1NormallyOpen.html',1,'Supla::Sensor']]],
+  ['notification_5',['Notification',['../classNotification.html',1,'Notification'],['../classSupla_1_1Notification.html',1,'Supla::Notification']]],
+  ['ntc10k_6',['NTC10k',['../classSupla_1_1Sensor_1_1NTC10k.html',1,'Supla::Sensor']]],
+  ['numericinputspec_7',['NumericInputSpec',['../structSupla_1_1NumericInputSpec.html',1,'Supla']]]
 ];

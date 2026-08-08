faa4b4f6d15125b38a339f51511526f222a3f35c
Marcin Bania <54779555+anarhbania@users.noreply.github.com>
2026-08-08 15:28:43 +0200

    Add LOG: show mac address

--- src/supla/network/ENC28J60.h
+++ src/supla/network/ENC28J60.h
@@ -73,6 +73,13 @@ class ENC28J60 : public Supla::Network {
                    dnsServerIP[1],
                    dnsServerIP[2],
                    dnsServerIP[3]);
+    SUPLA_LOG_INFO("ETH MAC: %02X:%02X:%02X:%02X:%02X:%02X",
+                   mac[0],
+                   mac[1],
+                   mac[2],
+                   mac[3],
+                   mac[4],
+                   mac[5]);
   }
 
  protected:

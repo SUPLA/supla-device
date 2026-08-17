bd2b8dc505a1ea21f7f1f3cbfe66bf7205a53cda
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2026-08-17 15:31:31 +0200

    Version increment 26.08.01

--- src/supla/version.h
+++ src/supla/version.h
@@ -4,7 +4,7 @@
 #ifndef SRC_SUPLA_VERSION_H_
 #define SRC_SUPLA_VERSION_H_
 
-#define SUPLA_SHORT_VERSION "26.08"
+#define SUPLA_SHORT_VERSION "26.08.01"
 const char suplaDeviceVersion[] = "SDK " SUPLA_SHORT_VERSION "-dev";
 
 #endif  // SRC_SUPLA_VERSION_H_

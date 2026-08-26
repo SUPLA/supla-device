d6d247c920460b810df1f3dc1af90bb4e0b698a1
klew <klew@users.noreply.github.com>
2026-08-26 09:10:05 +0000

    deploy: eeb31851b266edcea0acb8981a555b4da9a973d1

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['disablechannelstate_0',['disableChannelState',['../classSupla_1_1Element.html#aa46f400a295476e7cab0c2214ea304d6',1,'Supla::Element']]],
-  ['disablesensorschannelstate_1',['disableSensorsChannelState',['../classSupla_1_1Sensor_1_1MultiDsHandlerBase.html#ada5a09b3b4d5487087bcf642c7ced323',1,'Supla::Sensor::MultiDsHandlerBase']]]
+  ['defersleep_0',['deferSleep',['../classSuplaDeviceClass.html#a50ebdd9207a6833793f8d8b2274bc59a',1,'SuplaDeviceClass']]],
+  ['disablechannelstate_1',['disableChannelState',['../classSupla_1_1Element.html#aa46f400a295476e7cab0c2214ea304d6',1,'Supla::Element']]],
+  ['disablesensorschannelstate_2',['disableSensorsChannelState',['../classSupla_1_1Sensor_1_1MultiDsHandlerBase.html#ada5a09b3b4d5487087bcf642c7ced323',1,'Supla::Sensor::MultiDsHandlerBase']]]
 ];

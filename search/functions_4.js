7c953f8c550d4aa181d692131c2b601720e84e12
klew <klew@users.noreply.github.com>
2026-08-28 08:10:55 +0000

    deploy: 0fd84b63bd0d449f3671edf5156b81b82ec39334

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['end_0',['end',['../classSupla_1_1HtmlTag.html#af3b5aa7816e6f73a56a3fb24fe90db2e',1,'Supla::HtmlTag']]],
-  ['espeth_1',['ESPETH',['../classSupla_1_1ESPETH.html#ae161e3ecf5e68fc9bb953f16ea3d8324',1,'Supla::ESPETH::ESPETH(uint8_t ethMode)'],['../classSupla_1_1ESPETH.html#a5b77540799c810879bfec1ac922073a8',1,'Supla::ESPETH::ESPETH(eth_phy_type_t ethType, int32_t phyAddr, int mdc, int mdio, int power, eth_clock_mode_t clkMode)']]]
+  ['enablecyclicmode_0',['enableCyclicMode',['../classSupla_1_1Control_1_1Relay.html#a44e51bff93e90a830a974c3b698c1642',1,'Supla::Control::Relay']]],
+  ['end_1',['end',['../classSupla_1_1HtmlTag.html#af3b5aa7816e6f73a56a3fb24fe90db2e',1,'Supla::HtmlTag']]],
+  ['espeth_2',['ESPETH',['../classSupla_1_1ESPETH.html#ae161e3ecf5e68fc9bb953f16ea3d8324',1,'Supla::ESPETH::ESPETH(uint8_t ethMode)'],['../classSupla_1_1ESPETH.html#a5b77540799c810879bfec1ac922073a8',1,'Supla::ESPETH::ESPETH(eth_phy_type_t ethType, int32_t phyAddr, int mdc, int mdio, int power, eth_clock_mode_t clkMode)']]]
 ];

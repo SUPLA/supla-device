204995249451d13f875fa7870cd8fb90f1687c0e
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2026-09-15 13:57:08 +0200

    SuplaDevice: move CA certificates to a source file
    Move the public and private CA certificate definitions out of the header,
    expose them through extern declarations, and register the new source file
    with CMake.

--- cmake/SuplaDeviceSources.cmake
+++ cmake/SuplaDeviceSources.cmake
@@ -57,6 +57,7 @@ set(SUPLA_DEVICE_SRCS
   ${SUPLA_DEVICE_SRC_DIR}/supla/device/register_device.cpp
   ${SUPLA_DEVICE_SRC_DIR}/supla/device/factory_test.cpp
   ${SUPLA_DEVICE_SRC_DIR}/supla/device/security_logger.cpp
+  ${SUPLA_DEVICE_SRC_DIR}/supla/device/supla_ca_cert.cpp
 
   ${SUPLA_DEVICE_SRC_DIR}/supla/debug/command_processor.cpp
   ${SUPLA_DEVICE_SRC_DIR}/supla/debug/debug_log.cpp
